261efc8b5014a133efcdb48ccb59b8806936f4a7
SoumilAggarwal <aggarwal.soumil15@gmail.com>
2024-02-28 00:03:15 +0530

    finalCommit

--- Backend/utils/generateToken.js
+++ Backend/utils/generateToken.js
@@ -9,7 +9,7 @@ const generateTokenAndSetCookie = (userId, res) => {
     maxAge: 15 * 24 * 60 * 60 * 1000,
     httpOnly: true, //prevent XSS attacks (cross site scripting attacks)
     sameSite: "strict",
-    secure: process.env.NODE_ENV !== "development",
+    secure: process.env.NODE_ENV !== "production",
   });
 };
 
