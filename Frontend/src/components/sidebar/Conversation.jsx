45b31180bd75ca969311b02e80ac1e236602ab25
SoumilAggarwal <aggarwal.soumil15@gmail.com>
2024-02-27 13:12:23 +0530

    socketIo implemented successfully

--- Frontend/src/components/sidebar/Conversation.jsx
+++ Frontend/src/components/sidebar/Conversation.jsx
@@ -1,8 +1,11 @@
+import { useSocketContext } from "../../context/SocketContext";
 import useConversation from "../../zustand/useConversation";
-const Conversation = ({ conversation, lastIdx, emoji }) => {
+const Conversation = ({ conversation, lastIdx }) => {
   const { selectedConversation, setSelectedConversation } = useConversation();
 
   const isSelected = selectedConversation?._id === conversation._id;
+  const { onlineUsers } = useSocketContext();
+  const isOnline = onlineUsers.includes(conversation._id);
   return (
     <>
       <div
@@ -11,7 +14,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
         }`}
         onClick={() => setSelectedConversation(conversation)}
       >
-        <div className="avatar online">
+        <div className={`avatar ${isOnline ? "online" : ""}`}>
           <div className="w-12 rounded-full">
             <img src={conversation.profilePic} alt="user avatar" />
           </div>
@@ -20,7 +23,7 @@ const Conversation = ({ conversation, lastIdx, emoji }) => {
         <div className="flex flex-col flex-1">
           <div className="flex gap-7 justigy-between">
             <p className="font-bold text-gray-200">{conversation.fullName}</p>
-            <span className="text-xl">{emoji}</span>
+            {/* <span className="text-xl">{emoji}</span> */}
           </div>
         </div>
       </div>
